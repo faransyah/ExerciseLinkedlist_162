db5f2c94fbe9187acbcc6dff53ee2adc411fab20
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-06-09 10:31:28 +0700

    Add project files.

---
+++ ExerciseLinkedlist_162.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Node {
+public:
+	int rollNumber;
+	string name;
+	Node* next;
+};
+class CircularLinkedList {
+private:
+	Node* LAST;
+public:
+	CircularLinkedList() {
+		LAST = NULL;
+	}
+	void addNode();
+	bool search(int rollno, Node** previous, Node** current);
+	bool listEmpty();
+	bool delNode();
+	void traverse();
+};
+void CircularLinkedList::addNode() { // begining of the list
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama :";
+	cin >> nama;
+	nodeBaru->rollNumber = nim;
+	nodeBaru->name = nama;
+
+	if (LAST == NULL || nim <= LAST->rollNumber) {
+		if (LAST == NULL && nim <= LAST->rollNumber) {
+			cout << "\n nilai tidak di izinkan" << endl;
+			return;
+		}
+		nodeBaru->next = LAST;
+		if (LAST != NULL)
+			LAST->next = NULL;
+		nodeBaru->next = NULL;
+		LAST = nodeBaru;
+		return;
+	}
+	Node* current = current->next;
+	Node* previous = current;
+}
+bool CircularLinkedList::search(int rollno, Node** previous, Node** current) {
+	*previous = LAST->next;
+	*current = LAST->next;
+	while (*current != LAST) {
+		if (rollno == (*current)->rollNumber) {
+			return true;
+		}
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	if (rollno == LAST->rollNumber) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool CircularLinkedList::listEmpty() {
+	return LAST == NULL;
+}
+bool CircularLinkedList::delNode() { //between two nodes in the list
+	Node* current = current->next;
+	Node* previous = NULL;
+	if (search())
+	{
+
+	}
+}
+void CircularLinkedList::traverse() {
+	if (listEmpty()) {
+		cout << "\nList is empty\n";
+	}
+	else {
+		cout << "\nRecords in the list are:\n";
+		Node* currentNode = LAST->next;
+		while (currentNode != LAST) {
+			cout << currentNode->rollNumber << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+		cout << LAST->rollNumber << " " << LAST->name << endl;
+	}
+}
+int main() {
+	CircularLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all the records in the list" << endl;
+			cout << "4. Exit" << endl;
+			cout << "\nEnter your choice (1-5): ";
+			char ch;
+			cin >> ch;
+			switch (ch) {
+			case '1': {
+			}
+			case '2': {
+			}
+			case '3': {
+				obj.traverse();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}
